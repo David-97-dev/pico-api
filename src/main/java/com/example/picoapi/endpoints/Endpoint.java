cbe0e7152d328a86841a531271493ec4af33b0cd
David Bell <66362273+David-97-dev@users.noreply.github.com>
2022-07-11 20:29:23 +0100

    Add files via upload

---
+++ src/main/java/com/example/picoapi/endpoints/Endpoint.java
@@ -0,0 +1,22 @@
+package com.example.picoapi.endpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Endpoint {
+
+    @GetMapping("/")
+    String simpleResponse() {
+        return ("Hello pico w");
+    }
+
+    @PostMapping("/")
+    String simplePost(@RequestBody String string) {
+        System.out.println(string);
+        return ("Got you");
+    }
+
+}
---
+++ src/test/java/com/example/picoapi/PicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
