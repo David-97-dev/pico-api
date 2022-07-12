540a8906fe94f641295b1ae0d6ccff3f5edb21aa
David-97-dev <davidabell97@gmail.com>
2022-07-12 17:40:47 +0100

    modified endpoints

--- src/main/java/com/example/picoapi/endpoints/Endpoint.java
+++ src/main/java/com/example/picoapi/endpoints/Endpoint.java
@@ -1,22 +1,30 @@
 package com.example.picoapi.endpoints;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.time.Instant;
 
 @RestController
 public class Endpoint {
 
-    @GetMapping("/")
-    String simpleResponse() {
-        return ("Hello pico w");
+    @GetMapping("/status")
+    String statusCheck() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println("Client status request from IP: " + request.getRemoteAddr());
+        return ("[API] Communication Link Active");
     }
 
     @PostMapping("/")
     String simplePost(@RequestBody String string) {
-        System.out.println(string);
-        return ("Message Received");
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println("[" + request.getRemoteAddr() + "] " + string);
+        return ("[API] Message Received Timestamp: " + String.valueOf(Instant.now().getEpochSecond()));
     }
 
 }
