6de5b526a3d40c8226e7d4dfec7ead9891bd6a2e
david <davidabell97@gmail.com>
2022-07-12 21:01:49 +0100

    Added logger and UUID

--- src/main/java/com/example/picoapi/endpoints/Endpoint.java
+++ src/main/java/com/example/picoapi/endpoints/Endpoint.java
@@ -8,23 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 @RestController
 public class Endpoint {
 
+    List<String> list = new ArrayList<>();
+
+    Logger logger = Logger.getLogger(Endpoint.class.getName());
+
     @GetMapping("/status")
     String statusCheck() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        System.out.println("Client status request from IP: " + request.getRemoteAddr());
+        logger.info("Client status request from IP: " + request.getRemoteAddr());
         return ("[API] Communication Link Active");
+    }
+
+    @GetMapping("/getall")
+    String getAll() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        logger.info("Get all request from IP: " + request.getRemoteAddr());
+        return list.toString();
     }
 
     @PostMapping("/")
     String simplePost(@RequestBody String string) {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        System.out.println("[" + request.getRemoteAddr() + "] " + string);
-        return ("[API] Message Received Timestamp: " + String.valueOf(Instant.now().getEpochSecond()));
+        list.add(string);
+        UUID id = UUID.randomUUID();
+        logger.info("Message Received UUID:" + id);
+        return ("[API] Message Received UUID: " + id);
     }
 
 }
